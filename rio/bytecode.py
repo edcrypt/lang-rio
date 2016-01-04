9dd0a4b24ff1a99c8d5d1b32c42354add87b47d4
Eduardo de Oliveira Padoan <eduardo.padoan@gmail.com>
2016-01-04 14:09:11 -0200

    Beginning the compiler tests

--- rio/bytecode.py
+++ rio/bytecode.py
@@ -15,7 +15,7 @@ PUSH_NAME        2 (a)
 PUSH_NAME        3 (b)
 PUSH_CONST       0 (50)
 BUILD_TUPLE      3
-SEND_MSGWARGS    2
+BUILD_MSGWARGS   2
 
 Author: Eduardo de Oliveira Padoan
 Email:  eduardo.padoan@gmail.com
@@ -25,27 +25,36 @@ Email:  eduardo.padoan@gmail.com
 
 BYTECODES = [
     ### Compile-time emitted bytecodes
-    'POP_TOP',       # Remove top of the stack (TOS)
+    'POP_TOP',        # Remove top of the stack (TOS)
 
-    'PUSH_NAME',     # Push name #N from context to the stack
+    'PUSH_NAME',      # Push name #N from context to the stack
 
-    'PUSH_CONST',    # Push constant #N from context to the stack
+    'PUSH_CONST',     # Push constant #N from context to the stack
 
-    'BUILD_TUPLE',   # Build tuple with N itens
-                     # Remove items from stack, add tuple back
+    'BUILD_TUPLE',    # Build tuple with N itens
+                      # Remove items from stack, add tuple back
 
-    'SEND_MSGCHAIN', # Build simple message chain from N items
-                     # Remove items from stack, add msg result back
+    'BUILD_MSGCHAIN', # Build simple message chain from N items
+                      # Remove items from stack, add msg obj back
 
-    'SEND_MSGWARGS', # Build message chain from N items (except top of stack)
-                     # Plus top of stack as args tuple
-                     # Remove top + N items from stack, add msg result back
+    'BUILD_MSGWARGS', # Build message chain from N items (except top of stack)
+                      # Plus top of stack as args tuple
+                      # Remove top + N items from stack, add msg obj back
+
+    'SEND_MSG',       # Remove TOS, exec it and add msg result back in the stack
+
+    'RETURN_VALUE',   # End current block with TOS as message return value
 
     ### Bytecode emitted by 'primitive' methods
-    'JUMP_IF_FALSE',    # Jump forwards in the bytecode counter of TOS is False
-                        # Also add this point to a 'marks ring' on the context
+    #   Control flow
+    'JUMP_IF_FALSE',  # Jump forwards in the bytecode counter of TOS is False
+                      # Also add this point to a 'marks ring' on the context
 
-    'JUMP_BACKWARDS',   # Jump back to the latest mark on the ring
-
+    'JUMP_BACKWARDS', # Jump back to the latest mark on the ring
+    #   Algebra
+    'BINARY_ADD',
+    # TODO...
 ]
 
+def compile_ast(ast):
+    return
